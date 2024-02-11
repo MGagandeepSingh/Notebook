8a252c7acc6f62a27a181360f6e8f4d555e086d2
gagandeep-singh-maersk <g.singh@maersk.com>
2024-02-11 18:19:01 +0530

    Adding lombok configs to Employee class

--- Java/src/main/java/com/notebook/dto/Employee.java
+++ Java/src/main/java/com/notebook/dto/Employee.java
@@ -1,10 +1,14 @@
 package com.notebook.dto;
 
+import lombok.Builder;
+
 public record Employee(String firstName,
                        String lastName,
                        boolean isPermanent,
                        int salary) {
 
+    @SuppressWarnings("all")
+    @Builder(toBuilder = true)
     public Employee {
     }
 }
